6ed3d9851514793e13ba1fdf06bb452a0dce1d5f
bstrds <elapoueis@gmail.com>
2013-04-25 15:20:17 +0300

    fixd get on my lvl hoe

--- book.c
+++ book.c
@@ -32,50 +32,95 @@ void bookentry() {
 		return;
 	}
 	
-	for(;;) {
-		printf("New ISBN ");
-		gets(b_entries.isbn);
-		if(strlen(b_entries.isbn)==0) 
-		{
-			fclose(fp);
-			exit(0);
-		}
+	
+	printf("New ISBN: ");
+	
+	for(;;) 
+	{
+		scanf("%s", &b_entries.isbn);
 		
-		printf("New Title ");
-		gets(b_entries.title);
-		
-		printf("Copies Left ");
-		gets(str);
-		b_entries.copies = atoi(str);
-		
-		fwrite(&b_entries, sizeof(b_entries), 1, fp);
+		if(strlen(b_entries.isbn)==0 || strlen(b_entries.isbn)>13) 
+			printf("\nYou provided an invalid ISBN number\n");
+		else 
+			break;
 	}
-
+	
+	printf("New Title: ");
+	scanf("%s", &b_entries.title);
+	
+	printf("Copies Left: ");
+	scanf("%s", &str);
+	b_entries.copies = atoi(str);
+	
+	fwrite(&b_entries, sizeof(b_entries), 1, fp);
+	
+	fclose(fp);
 }
 
-main() {
+void lendentry() {
+	
+	system("clear");
+	
+	if((fp = fopen("LENDING_ENTRIES", "a+")) == NULL) 
+	{
+		printf("cannot open file\n");
+		return;
+	}
+	
+	printf("Enter ISBN: ");
+	
+	for(;;) 
+	{
+		scanf("%s", &l_entries.isbn);
+		
+		if(strlen(l_entries.isbn)==0 || strlen(l_entries.isbn)>13) 
+			printf("\nYou provided an invalid ISBN number\n");
+		else 
+			break;
+	}
+	
+	printf("Enter name: ");
+	scanf("%s", &l_entries.name);
+	
+	printf("Enter email: ");
+	scanf("%s", &l_entries.email);
+	
+	fwrite(&l_entries, sizeof(l_entries), 1, fp);
+	
+	fclose(fp);
+}
+
+int main(int argc, char *argv[]) {
 	
 	int choice; /* holds menu choice of user */
 	
-	system("clear"); 
-	
-	printf("1. Book Entry\n2. Lending Entry\n3. Print Book Statuses\n");
-	
-	scanf("%d", &choice);
-	
-	switch(choice)
+	for(;;)
 	{
-		case 1:	bookentry();
-				break;
-				
-		case 2: printf("@\n");
-				break;
-				
-		case 3: printf("#\n");
-				break;
+		system("clear"); 
+	
+		printf("1. Book Entry\n2. Lending Entry\n3. Print Book Statuses\
+		\n4. Exit\n");
 		
-		default: printf("u did some wrong\n");  
+		scanf("%d", &choice);
+		
+		switch(choice)
+		{
+			case 1:	bookentry();
+					break;
+					
+			case 2: lendentry();
+					break;
+					
+			case 3: printf("#\n");
+					break;
+					
+			case 4: printf("Thank you for using me\n");
+					exit(0);
+			
+			default: printf("u did some wrong\n");  
+		}
 	}
 	
+	return 0;
 	
 }
